4b4e6d8a0967aa42d26cfde183fe34d196d31d6c
jennyk1989 <jennifer0keene@gmail.com>
2021-12-07 18:15:35 -0600

    js start

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,41 @@
-//weather dashboard with form inputs (mostly HTML)
+//VARIABLES
+//button variables
+let searchBtn = $("#searchBtn");
+//weather condition variables:
+let currentCity = $("#currentCity");
+let currentTemp = $("#currentTemp");
+let currentWind = $("#currentWind");
+let currentHumidity = $("#currentHumidity");
+let currentUV = $("#currentUV");
+
+// Open Weather API
+const APIKey = "425535dc025827a7e77aa8a4d5289d87";
+
+// API parameter = "city"
+let currentWeather = function(city) {
+
+    //format the Open Weather API URL
+    let apiUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+
+    //fetch the weather from the OpenWeather URl
+    fetch(apiUrl)
+        //data fetched & returned in then() method
+        .then(function(response) {
+            //successful:
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data) {
+                    displayWeather(data, city);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        });
+}
 
 //search for a city, then presented with current & future condition
+
+
 
 //search for a city & it's added to search history
 
@@ -14,11 +49,23 @@
 //click on city in search history & then current & future conditions for that city are displayed
 
 //OPEN WEATHER API INFO
+/*how to use API KEYS
+1. request api key
+2. create variable to store API key (var APIKey = "......")
+3. create variables for the api call
+4. construct a query url to make the api call
+5. make the call using Fetch
+6. use the reponse data in your website 
+
+
+
+
+*/
 // API CALL = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 /* 
 API Calls
 -by city name --> api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
+my api key 425535dc025827a7e77aa8a4d5289d87
 
 API responses:
 city.name
