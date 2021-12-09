50bd23edd48a6441e0f96a98258aec69ab382cde
jennyk1989 <jennifer0keene@gmail.com>
2021-12-08 19:09:26 -0600

    error handling

--- assets/script.js
+++ assets/script.js
@@ -7,83 +7,72 @@ let currentTemp = $("#currentTemp");
 let currentWind = $("#currentWind");
 let currentHumidity = $("#currentHumidity");
 let currentUV = $("#currentUV");
+//search variables
+let searchHistory = $("#searchHistory");
+
+
+//local storage
+//localHistoryStorage =
 
 // Open Weather API
-const APIKey = "425535dc025827a7e77aa8a4d5289d87";
 
 // API parameter = "city"
-let currentWeather = function(city) {
-
-    //format the Open Weather API URL
-    let apiUrl = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-
-    //fetch the weather from the OpenWeather URl
+function getWeather (city) {
+    let apiKey = "425535dc025827a7e77aa8a4d5289d87";
+    let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=425535dc025827a7e77aa8a4d5289d87";
+    
     fetch(apiUrl)
-        //data fetched & returned in then() method
+        //url fetched & returned in then() method
         .then(function(response) {
-            //successful:
+            //if url successful:
             if (response.ok) {
                 console.log(response);
+                //json() method formats response as JSON, then captures the Promise into "data"
                 response.json().then(function(data) {
-                    displayWeather(data, city);
+                    console.log(data);
                 });
             } else {
-                alert("Error: " + response.statusText);
+                alert("Error: " + response.statusText); //alerts user of "Error: " + status message from response interface
             }
         });
+    
 }
 
-//search for a city, then presented with current & future condition
+//add city search button eventlistener
+$("#searchBtn").on("click", function () {
+    let city = $("#city").val(); //get the city name from input field
+    getWeather(city);
+    displayHistory();
+    $("#city").val(""); //clear it after getWeather function
+
+});
 
 
 
-//search for a city & it's added to search history
+let displayHistory = function(city, searchHistory) {
+    //check if city returned
+    if (city.length === 0) {
+        searchHistory.textContent = "No search history.";
+        return;
+    }
+    
+    //loop over history
+    for (var i=0; i < city.length; i++) {
+        //format city name
+        var cityName = city[i].name + "/" + city[i].name;
 
-//when viewing city's current weather conditions, see name, date, icon representing conditions, temp, humidity, wind speed, uv index
+        //creak link for each city
+        var cityLink = document.createElement("a");
+        cityLink.classList = "list-item flew-row justify-space-between align-center";
+        cityLink.setAttribute("href", "./weather?q=" + cityName);
 
-//when view uv index --> presented with a color that indicates favorable, moderate, or severe conditions
+        //span element to hold city name
+        var titleCity = document.createElement("span");
+        titleCity.textContent = cityName;
 
-//when viewing city's future weather conditions, see a 5 day forecast
-//5day forecast displays date, icon of weather conditions, temp, wind speed, humidity
+        //append to container
+        cityLink.appendChild(titleEl);
 
-//click on city in search history & then current & future conditions for that city are displayed
+    }
+}
 
-//OPEN WEATHER API INFO
-/*how to use API KEYS
-1. request api key
-2. create variable to store API key (var APIKey = "......")
-3. create variables for the api call
-4. construct a query url to make the api call
-5. make the call using Fetch
-6. use the reponse data in your website 
-
-
-
-
-*/
-// API CALL = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-/* 
-API Calls
--by city name --> api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-my api key 425535dc025827a7e77aa8a4d5289d87
-
-API responses:
-city.name
-
-current.temp 
-current.humidity
-current.uvi 
-current.wind_speed
-
-API ICONS:
-clear sky = 01d.png
-few clouds = 02d.png
-scattered clouds = 03d.png
-broken clouds = 04d.png
-shower rain = 09d.png
-rain = 10d.png
-thunderstorm = 11d.png
-snow = 13d.png
-mist = 50d.png
-
-*/
