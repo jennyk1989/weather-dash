0540ab7d0d3f5ac13e171418d3fa68f2342b2bb5
jennyk1989 <jennifer0keene@gmail.com>
2021-12-11 17:43:53 -0600

    search history appropriately shows and links work

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@
 let currentWeather = $("#currentWeather");
 //search variables
 let searchBtn = $("#searchBtn");
-let searchHistory = $("#searchHistory");
+let listofCities = $("#listofCities");
 //get out of local storage...store in array that's in addition to stored cities or in the empty array
 let cityHistory = JSON.parse(localStorage.getItem("city") || []); //parse stored data to get out of string format
     
@@ -38,7 +38,7 @@ function getWeather (city) {
 /*------------------ FUNCTION to take 'data' from getWeather put into useable variables to display ------------------*/
 function displayWeather(data) {
     //first clear previous data (if any)
-    //$(currentWeather).hide(); //make sure current weather is empty
+    $(currentWeather).empty(); 
     //variables to handles the city-specific data coming from the API
     let cityName = data.name; //takes "name" from the data object
     let tempLevel = JSON.stringify(data.main.temp);
@@ -51,14 +51,14 @@ function displayWeather(data) {
     $(currentWeather).append("<p>" + windSpeed + "</p>");
     $(currentWeather).append("<p>" + humidityLevel + "</p>");
     
-    
+    /*
     //weather icon 
     var weatherIcon = JSON.stringify(data.weather[0].icon);
     console.log(weatherIcon);
     let iconURL = new Image(); //adds image element 
     iconURL.src = "http://api.openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
     $(currentWeather).append(iconURL);
-    
+    */
 
 };
 
@@ -67,23 +67,23 @@ function displayWeather(data) {
     
 
     /*---------------displays cities searched in past-----------*/
-    function displayHistory() {
-        //empty the list first
-        $(searchHistory).empty();
-        //loop through storage until all stored cities are displayed (max of 6)
-        for (let i=0; i < cityHistory.length; i++) {
-            //searchHistory = id of div
-            //display city as a list item that links to weather of that city if clicked on
-            let listofCities = $(searchHistory).append("<a>" + cityHistory[i] + "</a>");
-            console.log(listofCities);
-             //append link into searchHistory container
-            $("a").addClass("list-item");//make the link display as a list item
-            //add an event listener if city in history is clicked 
-            $(listofCities).on("click", function() {
-                getWeather(cityHistory[i]);
+    function displayHistory(cityHistory) {
+        console.log(cityHistory);
+        cityHistoryCard.html = "";
+        $("#cityHistoryCard").empty();
 
-            })
+        //i = 1 to get rid of "null" value that keeps showing up 
+        for (let i=1; i < cityHistory.length; i++) {
+            let cityHistoryItem = $("<a>");
+            cityHistoryItem.addClass("list-group-item");//make the link display as a list item
+            //add city name to the link
+            cityHistoryItem.text(cityHistory[i]);
+            cityHistoryItem.attr("href");
+            $("#cityHistoryCard").append(cityHistoryItem);
+            
         }
+            
+        console.log(cityHistory);
     };
     
 /*------------------ CLICK listener...grabs user entry and sends it to getWeather via 'city' variable ------------------*/
@@ -91,7 +91,13 @@ function displayWeather(data) {
 $(searchBtn).on("click", function(event) {  
     event.preventDefault(); //prevent page from refreshing 
 
-    let city = $("#cityInput").val(); //get the city name from input field     
+    let city = $("#cityInput").val(); //get the city name from input field
+    
+    //if city input is empty, do nothing
+    if (city === "") {
+        return;
+    }
+
     getWeather(city); //send city to API to get weather 'data' 
     
     //send city to storage for later use 
@@ -105,13 +111,15 @@ $(searchBtn).on("click", function(event) {
     //store searched city into local storage --> localStorage.setItem (keyName, keyValue) per MDN
     //keyName = cityHistory , keyValue = value in city History
     localStorage.setItem(cityHistory, JSON.stringify(cityHistory));
-    displayHistory();
+    displayHistory(cityHistory);
 
 });
 
-//click listener for any clicks on the cityHistory list (listofCities)
-$(document).on("click", listofCities, function () { 
+//click listener for any clicks on the cityHistory list 
+$("#cityHistoryCard").on("click", ".list-group-item", function () { 
     //bring back up clicked city's weather conditions
-
+    //define variable to hold city's name
+    var searchedCity = $(this).text(); //export city's name as text
+    getWeather(searchedCity);
 });
 
